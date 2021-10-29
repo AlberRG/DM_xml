adf99cb395745e34aacec2f0ab172df91cf9bef1
Enrique de Aramburu <33132172+quiquear7@users.noreply.github.com>
2021-10-29 18:37:05 +0200

    Implementación de los dos xml

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from lxml import etree
 
-
-def leerXml():
-    doc = etree.parse('restaurantes_v1_es.xml')
+def parsearXML(xmlName, csvName):
+    doc = etree.parse(xmlName)
     raiz = doc.getroot()
     for i in raiz:
         for x in i:
             if x.find("name") is not None:
                 print(x.find("name").text)
+def leerXml():
+    print("Trabajando Restaurantes")
+    parsearXML('restaurantes_v1_es.xml', 'csv_restaurantes')
+    print("Trabajando Alojamientos")
+    parsearXML('alojamientos_v1_es.xml', 'csv_alojamientos')
 
 
 # Press the green button in the gutter to run the script.
